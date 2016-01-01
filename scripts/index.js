7a24b29578d166ed27b5572fb98d19d7bf6b3ca6
Bruno do Nascimento Maciel <brunonascimentomaciel@hotmail.com>
2016-01-01 05:48:57 -0200

    adicionado pagina de login

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
   alert("Este site se encontra em contrução. Caso encontre algum erro, envie um Email para brunonascimentomaciel@hotmail.com");
+  var usuario;
+  usuario= prompt("Digite o nome de Usuário:")
+  if(usuario=="paludin"){
+    var senha = prompt("Digite a Senha:")
+    if(senha == "senhadecuerola"){
+      location.href="desktop.htm";
+    }
+  }
 });
