a75f7cf07ddc5de9e5d3c1a3635508d7520b0278
Bruno do Nascimento Maciel <brunonascimentomaciel@hotmail.com>
2016-01-09 08:00:44 -0200

    Correção de bug
    corrigido bug que não permitia uma segunda exibição de elementos.

--- scripts/desktop.js
+++ scripts/desktop.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
   //escondendo os elementos
   $(".fundo_da_home").hide();
-  $(".seletor_janela_explorer_series").hide();
-  $(".lista_de_series_dubladas").hide();
+  //janela explorer
+  $(".janela_explorer").hide();
+  $(".titulo_da_janela_series").hide();
+  $(".lista_janela_series").hide();
+  $(".botao_de_fechar_series").hide();
+  //listas de series dubladas
+  $(".sherlock_serie_button").hide();
+  //lista de temporadas da serie sherlock dublada
+  $(".lista_de_temporadas_sherlock").hide();
+  //lista de episodios sherlock 1 temorada dublada
+  $(".lista_de_episodios_sherlock_01temporada").hide();
+
   //exibir e esconder o menu da home quando clicar no botão de home
   $(".botao_home").click(function(){
     $(".fundo_da_home").show(function(){
@@ -13,15 +23,39 @@ $(document).ready(function(){
   });
   //Exibir janela explorer series quando clicar em series na lista home
   $(".lista_da_home_series").click(function(){
-    $(".seletor_janela_explorer_series").show();
+    //janela explorer
+    $(".janela_explorer").show();
+    $(".titulo_da_janela_series").show();
+    $(".lista_janela_series").show();
+    $(".botao_de_fechar_series").show();
   });
   //fechar janela explorer series quando clicar no X
   $(".botao_de_fechar_series").click(function(){
-    $(".seletor_janela_explorer_series").hide();
+    //janela explorer
+    $(".janela_explorer").hide();
+    $(".titulo_da_janela_series").hide();
+    $(".lista_janela_series").hide();
+    $(".botao_de_fechar_series").hide();
+    //listas de series dubladas
+    $(".sherlock_serie_button").hide();
+    //lista de temporadas sherlock dublada
+    $(".lista_de_temporadas_sherlock").hide();
+    //lista de episódios sherlock 1 temporada
+    $(".lista_de_episodios_sherlock_01temporada").hide();
   });
   //exibir lista de series quando clicar em dubladas
   $(".botao_series_dubladas").click(function(){
-    $(".lista_de_series_dubladas").show();
+    $(".sherlock_serie_button").show();
     $(".lista_janela_series").hide();
+  });
+  //exibir listas de temporadas quando clicar em sherlock
+  $(".sherlock_serie_button").click(function(){
+    $(".sherlock_serie_button").hide();
+    $(".lista_de_temporadas_sherlock").show();
+  });
+  //exibir lista de episodios da primeira temporada quando clicar em 1 temporada (esconder lista de temporadas)
+  $(".button_01temporada_sherlock").click(function(){
+    $(".lista_de_episodios_sherlock_01temporada").show();
+    $(".lista_de_temporadas_sherlock").hide();
   });
 });
